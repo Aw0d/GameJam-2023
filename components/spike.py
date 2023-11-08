3b1bf5e1ca23cac70b147867b36ec3fcc6e5f153
SamiKZT <sami.kazi-tani@etu.univ-grenoble-alpes.fr>
2023-11-08 19:51:38 +0100

    changement de l'image du spike

--- components/spike.py
+++ components/spike.py
@@ -4,23 +4,26 @@ class Spike(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
 
-        size = 30 # Taille par défaut
+        # size = 30 # Taille par défaut
 
         # Définir la couleur du triangle (par exemple, rouge)
-        self.color = (255, 0, 0)
+        # self.color = (255, 0, 0)
 
-        # Créer la surface pour le triangle
-        self.image = pg.Surface((size, size))
-        self.image.fill((0, 0, 0, 0))  # Remplit la surface avec une couleur transparente
-        self.image.set_colorkey((0, 0, 0))  # Définit la couleur transparente
+        # # Créer la surface pour le triangle
+        # self.image = pg.Surface((size, size))
+        # self.image.fill((0, 0, 0, 0))  # Remplit la surface avec une couleur transparente
+        # self.image.set_colorkey((0, 0, 0))  # Définit la couleur transparente
 
-        # Coordonnées des sommets du triangle
-        p1 = (size // 2, 0)
-        p2 = (0, size)
-        p3 = (size, size)
+        # # Coordonnées des sommets du triangle
+        # p1 = (size // 2, 0)
+        # p2 = (0, size)
+        # p3 = (size, size)
 
-        # Dessiner le triangle sur la surface
-        pg.draw.polygon(self.image, self.color, [p1, p2, p3])
+        # # Dessiner le triangle sur la surface
+        # pg.draw.polygon(self.image, self.color, [p1, p2, p3])
+
+        # Créer une image pour le triangle
+        self.image = pg.image.load("images/malus/pencil.png")
 
         # Obtenir le rectangle englobant le triangle
         self.rect = self.image.get_rect()
