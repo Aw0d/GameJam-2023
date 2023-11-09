d352b60d94cbc11d1a180bfc7752332a10fe06a3
8enjam1n <benjaminkalic131@gmail.com>
2023-11-09 19:12:00 +0100

    ajout classe level

--- level/level.py
+++ level/level.py
@@ -1,21 +1,15 @@
 import pygame as pg
 
 class Level:
-    def __init__(self, name , background="../images/background2.png"):
+    def __init__(self, name):
         self.name = name
-        self.background = background
         #liste des positions des éléments d'une map
         self.contents = []
 
     def setName(self, name):
         self.name  = name
     
-    def setBackground(self, background):
-        self.background = background
-    
     def addContent(self, content):
         self.contents.append(content)
 
-    def deleteContent(self, content):
-        self.contents.remove(content)
-    
+
