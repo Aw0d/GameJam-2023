952d3ae254a711982e3fd4b1ae3bec634a40ef31
Aw0d <awod2004@yahoo.com>
2023-11-09 19:38:02 +0100

    Modification de la classe level

--- level/level.py
+++ level/level.py
@@ -3,13 +3,19 @@ import pygame as pg
 class Level:
     def __init__(self, name):
         self.name = name
-        #liste des positions des éléments d'une map
-        self.contents = []
+        # liste des positions des éléments d'une map
+        # La clé correspond à l'objet
+        # La valeur est un tuple contenant les coordonnées
+        # Pour un Ground, c'est une liste contenant deux tuples, la position et la taille
+        # exemple : {
+        #   "Chair": (10, 10),
+        #   "Table": (60, 10),
+        #   "Ground": [(100, 10), (50, 50)]
+        # }
+        self.all = {}
+        self.endGame = None
 
     def setName(self, name):
         self.name  = name
-    
-    def addContent(self, content):
-        self.contents.append(content)
 
 
